6568595d1fe25fb313f7638884abbe194fdee999
Msirous <MSirouss@gmail.com>
2017-02-11 22:54:45 +0330

    chenge localhost to another host

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '148.251.173.41'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'toranjcms_toranjcars'),
             'username' => env('DB_USERNAME', 'toranjcms_root'),
