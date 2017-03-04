9fe6cce1ef11c756a0e2fcb16ce687c1730b22e8
Msirous <MSirouss@gmail.com>
2017-03-04 12:47:23 +0330

    test

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,13 +1,12 @@
-<h1>ساخت کاربر جدید</h1>
+<h1>ساخت یک مجموعه</h1>
 
-{!! Form::open(['method' => 'POST' , 'action' => "route('catStore',[Request::get('parent_id',1)])" , 'files' =>true]) !!}
-  <div class="form-group">
-    {!! Form::label('title' , 'نام و نام خانوادگی') !!}
+{!! Form::open(['method' => 'POST' , 'files' =>true]) !!}  <div class="form-group">
+    {!! Form::label('title' , 'عنوان') !!}
     {!! Form::text('title' ,null, ['class' =>'form-control']) !!}
 
  </div>
   <div class="form-group">
-    {!! Form::submit('ایجاد کاربر جدید' ,['class' =>'btn btn-primary']) !!}
+    {!! Form::submit('افزودن' ,['class' =>'btn btn-primary']) !!}
   </div>
 
 {!! Form::close() !!}
