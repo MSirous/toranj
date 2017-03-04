a79e6776dd3f04134ae245d886a75364d769535e
Msirous <MSirouss@gmail.com>
2017-03-04 12:41:45 +0330

    chenge categories

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,12 +1,11 @@
 <h1>ساخت کاربر جدید</h1>
+
 {!! Form::open(['method' => 'POST' , 'action' => "route('catStore',[Request::get('parent_id',1)])" , 'files' =>true]) !!}
   <div class="form-group">
     {!! Form::label('title' , 'نام و نام خانوادگی') !!}
     {!! Form::text('title' ,null, ['class' =>'form-control']) !!}
 
-    {!! Form::label('state' , 'محل اقامت') !!}
-{{--     {!! Form::select('cgroup_id' , App\Locate::pluck('title','id'), null, ['class' =>'form-control']) !!}
- --}}  </div>
+ </div>
   <div class="form-group">
     {!! Form::submit('ایجاد کاربر جدید' ,['class' =>'btn btn-primary']) !!}
   </div>
