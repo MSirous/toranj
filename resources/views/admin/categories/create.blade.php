d9341fbcfc368e85eb84fe40df5df54e392ce5df
Iman <imanghafoori1@gmail.com>
2017-03-04 12:46:26 +0330

    Update create.blade.php

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,6 +1,6 @@
 <h1>ساخت کاربر جدید</h1>
 
-{!! Form::open(['method' => 'POST' , 'action' => "route('catStore',[Request::get('parent_id',1)])" , 'files' =>true]) !!}
+{!! Form::open(['method' => 'POST' , 'action' => route('catStore',[Request::get('parent_id',1)]) , 'files' =>true]) !!}
   <div class="form-group">
     {!! Form::label('title' , 'نام و نام خانوادگی') !!}
     {!! Form::text('title' ,null, ['class' =>'form-control']) !!}
